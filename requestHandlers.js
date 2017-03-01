9a11bd2ee07f802e78536b940c68cd9cfd8ed550
xueweiyema@yahoo.com.cn <123456>
2017-03-01 11:28:32 +0800

    add picture upload

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,17 +1,21 @@
 let exec = require("child_process").exec
 let querystring = require("querystring")
+let formidable = require("formidable")
+let fs = require("fs")
+
 
 function start(response, postData) {
     console.log("Request handler 'start' was called.");
     var body = '<html>' +
         '<head>' +
-        '<meta http-equiv="Content-Type" content="text/html; ' +
-        'charset=UTF-8" />' +
+        '<meta http-equiv="Content-Type" ' +
+        'content="text/html; charset=UTF-8" />' +
         '</head>' +
         '<body>' +
-        '<form action="/upload" method="post">' +
-        '<textarea name="text" rows="20" cols="60"></textarea>' +
-        '<input type="submit" value="Submit text" />' +
+        '<form action="/upload" enctype="multipart/form-data" ' +
+        'method="post">' +
+        '<input type="file" name="upload">' +
+        '<input type="submit" value="Upload file" />' +
         '</form>' +
         '</body>' +
         '</html>'
@@ -22,14 +26,51 @@ function start(response, postData) {
     response.end();
 }
 
-function upload(response, postData) {
+function upload(response, request) {
     console.log("Request handler 'upload' was called.")
-    response.writeHead(200, {
-        "Content-Type": "text/plain"
+    let form = new formidable.IncomingForm()
+    form.uploadDir = 'tmp'
+    console.log("about to parse")
+    form.parse(request, function (error, fields, files) {
+        console.log("parsing done")
+        fs.renameSync(files.upload.path, './tmp/test.jpg')
+
+        // var readStream = fs.createReadStream(files.upload.path);
+        // var writeStream = fs.createWriteStream("./tmp/test.jpg");
+        // readStream.pipe(writeStream);
+        // readStream.on('end', function () {
+        //     fs.unlinkSync(files.upload.path);
+        // });
+
+
+        response.writeHead(200, {
+            "Content-Type": "text/html"
+        })
+        response.write("received image:<br/>")
+        response.write("<img src='/show' />")
+        response.end()
     })
-    response.write("You've sent:" + querystring.parse(postData).text)
-    response.end()
+}
+
+function show(response) {
+    console.log("Request handler 'show' was called.")
+    fs.readFile("./tmp/test.jpg", "binary", function (error, file) {
+        if (error) {
+            response.writeHead(500, {
+                "Content-Type": "text/plain"
+            })
+            response.write(error + "\n")
+            response.end()
+        } else {
+            response.writeHead(200, {
+                "Content-Type": "image/jpg"
+            })
+            response.write(file, "binary")
+            response.end()
+        }
+    })
 }
 
 exports.start = start
 exports.upload = upload
+exports.show = show
--- server.js
+++ server.js
@@ -1,22 +1,12 @@
 let http = require("http")
 let url = require("url")
+let formidable = require('formidable')
+let util = require("util")
 
 function start(route, handle) {
     function onRequest(request, response) {
-        let postData = ""
         let pathname = url.parse(request.url).pathname
-        console.log("Request for " + pathname + " received.")
-        request.setEncoding("utf8")
-        request.addListener("data", function (postDataChunk) {
-            // called when a new chunk of data was received
-            postData += postDataChunk
-            console.log("Received POST data chunk '" +
-                postDataChunk + "'.")
-        })
-        request.addListener("end", function () {
-            // called when all chunks of data have been received
-            route(handle, pathname, response, postData)
-        })
+        route(handle, pathname, response, request)
     }
     http.createServer(onRequest).listen(8888)
     console.log("Server has started.")
